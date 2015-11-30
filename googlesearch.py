b90439179ed936e0d68ed047cfd8af3bfa9a643a
udothemath1984 <udothemath1984@Pros-MacBook-Pro.local>
2015-11-30 16:25:25 -0500

    v0
    upload

---
+++ googlesearch.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 30 14:02:48 2015
+
+@author: Tonnam
+"""
+# http://stackoverflow.com/questions/1657570/google-search-from-a-python-app
+
+import json
+import urllib
+
+def googlesearch(searchfor):
+  query = urllib.urlencode({'q': searchfor})
+  url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&%s' % query
+  search_response = urllib.urlopen(url)
+  search_results = search_response.read()
+  results = json.loads(search_results)
+  data = results['responseData']
+  print 'Total results: %s' % data['cursor']['estimatedResultCount']
+  hits = data['results']
+  print 'Top %d hits:' % len(hits)
+  for h in hits: print ' ', h['url']
+  print 'For more results, see %s' % data['cursor']['moreResultsUrl']
---
+++ test_googlesearch.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 30 14:39:05 2015
+
+@author: Tonnam
+"""
+
+from googlesearch import googlesearch
+
+googlesearch('Ag melting point')
